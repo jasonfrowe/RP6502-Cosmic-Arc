64da8bdce81ea88f801df0e99273735d168b47f6
Jason Rowe <jasonfrowe@gmail.com>
2026-04-10 08:56:22 -0400

    Add ESC key functionality to exit the game cleanly

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "beasties.h"
 #include "palette.h"
 #include "input.h"
+#include "usb_hid_keys.h"
 #include "score.h"
 #include "opl.h"
 #include "lander.h"
@@ -809,6 +810,9 @@ int main(void)
 
         // Get Input from Keyboard/Gamepad
         handle_input();
+
+        // ESC exits the game cleanly
+        if (key(KEY_ESC)) return 0;
 
         if (game_mode == GAME_MODE_DEMO) {
             bool fire_pressed = is_action_pressed(0, ACTION_FIRE);
